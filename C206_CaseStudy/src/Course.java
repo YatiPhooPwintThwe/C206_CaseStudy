de5350285350cc1002c3707897bcea7840053219
22015024 <22015024@10.175.6.147>
2023-08-11 12:11:11 +0800

    Course

---
+++ C206_CaseStudy/src/Course.java
@@ -0,0 +1,58 @@
+import java.time.LocalTime;
+
+public class Course {
+
+	private String code;
+	private String title;
+	private String instructor;
+	private String day;
+	private String time;
+
+	public Course(String code, String title, String instructor, String day, String time) {
+		this.code = code;
+		this.title = title;
+		this.instructor = instructor;
+		this.day = day;
+		this.time = time;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(String instructor) {
+		this.instructor = instructor;
+	}
+
+	public String getDay() {
+		return day;
+	}
+
+	public void setDay(String day) {
+		this.day = day;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+}
