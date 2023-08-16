2e09ca31c30a224b4e6ce0af0a652a3b1d44c3c9
Yati Phoo Pwint Thwe <Yati Phoo Pwint Thwe@Yati>
2023-08-17 00:04:43 +0800

    committed by yati
    yati

---
+++ C206_CaseStudy/src/Enrolment.java
@@ -0,0 +1,26 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Yati Phoo Pwint Thwe
+ * Student ID: 22003299
+ * Class: W65N
+ * Date/Time created: Wednesday 16-08-2023 23:52
+ */
+
+/**
+ * @author Yati Phoo Pwint Thwe
+ *
+ */
+public class Enrolment {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
